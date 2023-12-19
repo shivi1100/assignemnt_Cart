a240446c9138d9b467839153bf9f1ee8a9ebb622
shivi1100 <147822546+shivi1100@users.noreply.github.com>
2023-12-19 22:51:13 +0530

    js_changes

--- cart.js
+++ cart.js
@@ -1,9 +1,29 @@
 function toggle(){
-    const card = document.getElementById('pairOne');
-    if(card.style.display === "hidden") {
+    const card = document.querySelector('table');
+    if(card.style.display === "none") {
         card.style.display = "block";
     }
     else{
         card.style.display = "none";
+    }
+}
+
+function toggleTwo(){
+    const cardOne = document.querySelector('table.pairTwo')
+    if(cardOne.style.display === "none") {
+        cardOne.style.display = "block";
+    }
+    else{
+        cardOne.style.display = "none"
+    }
+}
+
+function toggleOne(){
+    const cardTwo = document.querySelector('table.pairThree')
+    if(cardTwo.style.display === "none") {
+        cardTwo.style.display = "block";
+    }
+    else{
+        cardTwo.style.display = "none"
     }
 }
